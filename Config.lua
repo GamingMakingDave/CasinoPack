3456c0d86d797239e43c35dfa16b87022e792d3b
Dave <griefer.daveyt@gmail.com>
2022-08-11 04:05:23 +0200

    Add Configs and Locals

--- Config.lua
+++ Config.lua
@@ -1,3 +1,21 @@
-Config{
-    WheelCost = 5000
+Config = {}
+Config.Locale = 'de'
+
+Config.WheelCost = 5000
+
+Config.Carmodel = 'furia'
+Config.CasinoCarPos = {
+    Spawn = {
+        x = 963.09,
+        y = 48.91,
+        z = 75.57
+    },
+}
+
+
+Config.Language = {
+    ['de'] = {
+        ['YouHave'] = 'Du hast _money Gewonnen',
+        ['Win'] = 'gewonnen!',
+    }
 }
